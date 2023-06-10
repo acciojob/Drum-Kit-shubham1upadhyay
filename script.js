3d1b20da7c341aa797b453609f753803479055d3
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-10 13:25:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function playSound(key_code) {
+  const audio = document.querySelector(`audio[data-key="${key_code}"]`);
+  const key = document.querySelector(`.key[data-key="${key_Code}"]`);
+  
+  if(audio)
+  {
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
+
+document.addEventListener('click', (event) => {
+  playSound(event.keyCode);
+});
